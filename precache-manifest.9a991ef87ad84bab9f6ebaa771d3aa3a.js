89716f4b5e4a2c8264cdbb8c1834edde0d495c06
kaurjasleen <jas.kaur97@gmail.com>
2020-03-24 17:58:19 +0530

    Updates

---
+++ precache-manifest.9a991ef87ad84bab9f6ebaa771d3aa3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c892863a9d5cd610d6b3",
+    "url": "/static/js/main.c892863a.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c892863a9d5cd610d6b3",
+    "url": "/static/css/main.7bb4cfdb.chunk.css"
+  },
+  {
+    "revision": "0a74f1da9d241dfafceaf33dd6cc3512",
+    "url": "/index.html"
+  }
+];
